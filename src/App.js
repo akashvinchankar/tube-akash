dd2f88ce9dc130d21f3ae4618905324f6d06a996
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2023-12-24 18:14:36 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import Body from "./components/Body";
+import Head from "./components/Head";
+
+function App() {
+  return (
+    <div>
+      <Head />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
+
+// eslint-disable-next-line no-lone-blocks
+{
+  /* 
+Head
+Body
+  Sidebar
+    MenuItems
+  MainContainer
+    ButtonsList
+    VideoContainer
+      VideoCard
+*/
+}
