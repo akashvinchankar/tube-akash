17f205bda7a53a3940e466e245d2e93fb61ab9c3
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2023-12-25 17:22:44 +0530

    Refactor ButtonList component to add horizontal scrolling and accordion functionality

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,9 @@ import React from "react";
 const Button = ({ name }) => {
   return (
     <div>
-      <button className="px-5 py-1 m-2 bg-gray-200 rounded-lg">{name}</button>
+      <button className="px-5 py-1 m-2 bg-gray-200 rounded-lg whitespace-nowrap">
+        {name}
+      </button>
     </div>
   );
 };
--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import Button from "./Button";
 
 const buttons = [
@@ -15,12 +15,45 @@ const buttons = [
   { id: 11, name: "360° Video" },
 ];
 
+// TODO: Make this buttons like accordion (when you click on one button, it will open and show you more buttons)
+
 const ButtonList = () => {
+  const containerRef = useRef(null);
+
+  const scrollLeft = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollLeft -= 70;
+    }
+  };
+
+  const scrollRight = () => {
+    if (containerRef.current) {
+      containerRef.current.scrollLeft += 70;
+    }
+  };
+
   return (
-    <div className="flex flex-wrap">
-      {buttons.map((button) => (
-        <Button key={button.id} name={button.name} />
-      ))}
+    <div className="flex items-center overflow-x-hidden">
+      <button
+        className="px-2 py-1 bg-gray-200 rounded-l focus:outline-none transition-all duration-300"
+        onClick={scrollLeft}
+      >
+        &lt;
+      </button>
+      <div
+        className="flex flex-nowrap space-x-2 overflow-x-auto max-w-screen-lg transition-all duration-300"
+        ref={containerRef}
+      >
+        {buttons.map((button) => (
+          <Button key={button.id} name={button.name} />
+        ))}
+      </div>
+      <button
+        className="px-2 py-1 bg-gray-200 rounded-r focus:outline-none transition-all duration-300"
+        onClick={scrollRight}
+      >
+        &gt;
+      </button>
     </div>
   );
 };
