9d6832a49a9b222153be83483a8dfc7114556efc
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2023-12-26 00:35:47 +0530

    Refactor Sidebar component styling and structure

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -10,29 +10,41 @@ const Sidebar = () => {
   }
 
   return (
-    <div className="p-5 shadow-lg w-48">
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
+    <div className="p-5 shadow-lg w-48 bg-white">
+      <ul className="list-none p-0 m-0">
+        <li className="mb-2">
+          <Link to="/" className="text-gray-800 hover:text-black font-medium">
+            Home
+          </Link>
         </li>
-        <li>Shorts</li>
-        <li>Videos</li>
-        <li>Live</li>
+        <li className="text-gray-600 hover:text-black">Shorts</li>
+        <li className="text-gray-600 hover:text-black">Videos</li>
+        <li className="text-gray-600 hover:text-black">Live</li>
       </ul>
-      <h1 className="font-bold pt-5">Subscriptions</h1>
-      <ul>
-        <li>Music</li>
-        <li>Sports</li>
-        <li>Gaming</li>
-        <li>Movies</li>
-      </ul>
-      <h1 className="font-bold pt-5">Watch Later</h1>
-      <ul>
-        <li>Music</li>
-        <li>Sports</li>
-        <li>Gaming</li>
-        <li>Movies</li>
-      </ul>
+
+      <div className="border-t border-gray-300 my-3"></div>
+
+      <div>
+        <h1 className="font-bold text-gray-800">Subscriptions</h1>
+        <ul className="list-none p-0 m-0">
+          <li className="text-gray-600 hover:text-black">Music</li>
+          <li className="text-gray-600 hover:text-black">Sports</li>
+          <li className="text-gray-600 hover:text-black">Gaming</li>
+          <li className="text-gray-600 hover:text-black">Movies</li>
+        </ul>
+      </div>
+
+      <div className="border-t border-gray-300 my-3"></div>
+
+      <div>
+        <h1 className="font-bold text-gray-800">Watch Later</h1>
+        <ul className="list-none p-0 m-0">
+          <li className="text-gray-600 hover:text-black">Music</li>
+          <li className="text-gray-600 hover:text-black">Sports</li>
+          <li className="text-gray-600 hover:text-black">Gaming</li>
+          <li className="text-gray-600 hover:text-black">Movies</li>
+        </ul>
+      </div>
     </div>
   );
 };
