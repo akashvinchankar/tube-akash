5a771075138d7fa765f0de9dc69770c779947baf
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2023-12-25 23:46:40 +0530

    Refactor ButtonList component to remove unnecessary code and improve UI

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React from "react";
 import Button from "./Button";
 
 const buttons = [
@@ -16,44 +16,12 @@ const buttons = [
 ];
 
 // TODO: Make this buttons like accordion (when you click on one button, it will open and show you more buttons)
-
 const ButtonList = () => {
-  const containerRef = useRef(null);
-
-  const scrollLeft = () => {
-    if (containerRef.current) {
-      containerRef.current.scrollLeft -= 70;
-    }
-  };
-
-  const scrollRight = () => {
-    if (containerRef.current) {
-      containerRef.current.scrollLeft += 70;
-    }
-  };
-
   return (
-    <div className="flex items-center overflow-x-hidden">
-      <button
-        className="px-2 py-1 bg-gray-200 rounded-l focus:outline-none transition-all duration-300"
-        onClick={scrollLeft}
-      >
-        &lt;
-      </button>
-      <div
-        className="flex flex-nowrap space-x-2 overflow-x-auto max-w-screen-lg transition-all duration-300"
-        ref={containerRef}
-      >
-        {buttons.map((button) => (
-          <Button key={button.id} name={button.name} />
-        ))}
-      </div>
-      <button
-        className="px-2 py-1 bg-gray-200 rounded-r focus:outline-none transition-all duration-300"
-        onClick={scrollRight}
-      >
-        &gt;
-      </button>
+    <div className="flex flex-wrap">
+      {buttons.map((button) => (
+        <Button key={button.id} name={button.name} />
+      ))}
     </div>
   );
 };
