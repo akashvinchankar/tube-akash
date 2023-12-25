dc2ac806c00c37dc30c7e342042288bb9ec60688
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2023-12-26 00:32:17 +0530

    Add useEffect hook and dispatch closeMenu action in WatchPage component

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../utils/appSlice";
+import { useSearchParams } from "react-router-dom";
 
 const WatchPage = () => {
-  return <div>WatchPage</div>;
+  const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
+
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
+
+  return (
+    <div>
+      <iframe
+        className="mt-5 mx-5 rounded-xl shadow-xl"
+        width="800"
+        height="500"
+        src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+    </div>
+  );
 };
 
 export default WatchPage;
