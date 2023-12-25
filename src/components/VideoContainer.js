fc279caef3d835cf432338011981ba02a4d24e0a
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2023-12-26 00:32:08 +0530

    Add link to video details page in VideoContainer

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { YOUTUBE_VIDEO_API } from "../utils/constants";
 import VideoCard from "./VideoCard";
+import { Link } from "react-router-dom";
 
 const VideoContainer = () => {
   const [videos, setVideos] = useState([]);
@@ -24,7 +25,9 @@ const VideoContainer = () => {
       <h2 className="text-2xl font-bold text-gray-800 ml-4">Latest Videos</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {videos.map((video) => (
-          <VideoCard key={video.id.videoId} video={video} />
+          <Link to={"/watch?v="+video.id}>
+            <VideoCard key={video.id.videoId} video={video} />
+          </Link>
         ))}
       </div>
     </div>
