b8acda40dd9711e7f454203e7de9c0112b1be936
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2023-12-25 16:29:09 +0530

    Add Redux and React Redux dependencies

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const Head = () => {
   return (
-    <div>Head</div>
-  )
-}
+    <div className="grid grid-flow-col p-2 m-2 shadow-lg">
+      <div className="flex col-span-1">
+        <img
+          className="h-12 w-12"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1024px-Hamburger_icon.svg.png"
+          alt="hamburger-icon"
+        />
+        <img
+          className="h-12 mx-2"
+          src="https://freebiehive.com/wp-content/uploads/2023/04/Youtube-Logo-PNG-1.jpg"
+          alt="youtube-logo"
+        />
+      </div>
+      <div className="col-span-10 text-center">
+        <input className="w-1/2 border border-gray-400 p-2 rounded-l-full" type="text" placeholder="Search" />
+        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">🔍</button>
+      </div>
+      <div className="col-span-1">
+        <img
+          className="h-12 w-12"
+          src="https://w7.pngwing.com/pngs/178/595/png-transparent-user-profile-computer-icons-login-user-avatars-thumbnail.png"
+          alt="avatar"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Head
+export default Head;
