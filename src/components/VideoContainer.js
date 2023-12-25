ea1396199ddc1ad3b18dbcd1b88868061ec01ce1
Akash Vinchankar <86399324+akashvinchankar@users.noreply.github.com>
2023-12-25 23:46:56 +0530

    Add video fetching functionality and display latest videos

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { YOUTUBE_VIDEO_API } from "../utils/constants";
+import VideoCard from "./VideoCard";
 
 const VideoContainer = () => {
-  return <div>VideoContainer</div>;
+  const [videos, setVideos] = useState([]);
+
+  const getVideos = async () => {
+    try {
+      const response = await fetch(YOUTUBE_VIDEO_API);
+      const data = await response.json();
+      setVideos(data.items);
+    } catch (error) {
+      console.error("Error fetching videos:", error);
+    }
+  };
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold text-gray-800 ml-4">Latest Videos</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {videos.map((video) => (
+          <VideoCard key={video.id.videoId} video={video} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default VideoContainer;
